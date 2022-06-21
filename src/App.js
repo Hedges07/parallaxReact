97125b53fd7e6985798c82cb824ca1a307dee4e7
Hedges07 <57685547+Hedges07@users.noreply.github.com>
2022-06-21 00:13:28 -0400

    Parallax

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import { useRef } from 'react';
+import moon from './img/moon.png';
+import land from './img/land.png';
+import cat from './img/cat.gif';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
 function App() {
+  const ref = useRef();
+
   return (
-    <div className="App">
-     <Parallax pages = {4}>
+    <div>
+      <Parallax pages={4} ref={ref}>
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
 
         <ParallaxLayer
-          speed = {1}
-          offset = {0}
-          factor = {2}
-          style = {{
-            backgroundImage: 'url: img(${sky})',
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
             backgroundSize: 'cover',
           }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
         >
-          <h2>
-            This is Parallax
-          </h2>
+          <img src={cat} />
         </ParallaxLayer>
 
-        <ParallaxLayer 
-          offset={2} 
-          speed = {.5}
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
         >
-          <h2>
-            This is Parallax
-          </h2>
         </ParallaxLayer>
 
-     </Parallax>
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+        >
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
+
 export default App;
